9dcb9bdc6bdfdce06bb280181628d1f724c681f6
Kevin Young <youngkevinnc@gmail.com>
2025-12-03 16:10:55 -0500

    updating text

--- src/components/BodySection.jsx
+++ src/components/BodySection.jsx
@@ -6,9 +6,10 @@ export default function BodySection() {
     <>
       <section className="intro-text">
         <p>
-        The anatomy of the sarracenia flower is consistent throughout the genus - however there are difference in floral coloration, 
-        size, proportion, and scent. All these traits are useful when identifying a species, and can be helpful when trying to distinguish 
-        the parentage in a complex hybrid. 
+        Few plants have evolved such a complex pollination scheme. Their one-way pollination pathway strongly favors cross-pollination (or 'outcrossing'), 
+        and many researchers believe this is a major reason pitcher plants show such a range in morphological diversity. Ironically, 
+        this reliance on cross-pollination also means the genus carries a heavy genetic load. When self-pollination 
+        does occur, those hidden deleterious genes are suddenly expressed, leading to rapid inbreeding depression.
         </p>
       </section>
 
